287f61a0b8bddac38ffb19aac128bed211d7b82d
FanFan0919 <vincent0919@nyu.edu>
2019-10-23 17:48:57 -0400

    burger-builder
    checkout page of the app

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -89,29 +89,21 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // Use a POST to send a dummy order
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Yifan Zhang',
-                address: {
-                    street: 'test street 1',
-                    zipcode: '06103',
-                    country: 'United States'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            // queryParams.push('meat=1')
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-            });
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: './checkout',
+            search: '?' + queryString
+        });
+
+
+        
     }
 
     render () {
